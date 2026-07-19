b4bf6c0bb56c2fe90c9bb9f4ddfd067b1be30051
rudhrapriya_s <rudhrasirini2006@gmail.com>
2026-07-19 08:19:02 +0530

    Create

---
+++ problem-1784429341501/version/python/problem-1784429341501_20260719_081901.py
@@ -0,0 +1,4 @@
+# Last updated: 7/19/2026, 8:19:01 AM
+1class Solution:
+2    def canReach(self,start:list[int],target:list[int]) -> boot:
+3        return (start[0] + start [1]) % 2 == (target[0] + target[1])%2
