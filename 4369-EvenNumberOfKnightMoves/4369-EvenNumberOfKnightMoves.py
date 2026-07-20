6a325ef36e60253c8c9e3711f37089877f8e586b
rudhrapriya_s <rudhrasirini2006@gmail.com>
2026-07-20 10:12:29 +0530

    Create 4369-EvenNumberOfKnightMoves

---
+++ 4369-EvenNumberOfKnightMoves/4369-EvenNumberOfKnightMoves.py
@@ -0,0 +1,4 @@
+# Last updated: 7/20/2026, 10:12:29 AM
+class Solution:
+    def canReach(self,start:list[int],target:list[int]) -> boot:
+        return (start[0] + start [1]) % 2 == (target[0] + target[1])%2
