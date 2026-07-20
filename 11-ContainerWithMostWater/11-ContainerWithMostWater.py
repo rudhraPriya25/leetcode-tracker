c51ff63be67b8647fa28b78ef5b18cb139664659
rudhrapriya_s <rudhrasirini2006@gmail.com>
2026-07-20 10:15:19 +0530

    Create 11-ContainerWithMostWater

---
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -0,0 +1,17 @@
+# Last updated: 7/20/2026, 10:15:19 AM
+class Solution:
+    def maxArea(self, height):
+        left, right = 0, len(height) - 1
+        max_area = 0
+
+        while left < right:
+            h = min(height[left], height[right])
+            w = right - left
+            max_area = max(max_area, h * w)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area    
