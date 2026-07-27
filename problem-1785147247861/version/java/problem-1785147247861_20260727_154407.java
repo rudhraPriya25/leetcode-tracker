c0c8cd50a0d9d7082be0da36961899b52db2b9c4
rudhrapriya_s <rudhrasirini2006@gmail.com>
2026-07-27 15:44:11 +0530

    Create

---
+++ problem-1785147247861/version/java/problem-1785147247861_20260727_154407.java
@@ -0,0 +1,15 @@
+// Last updated: 7/27/2026, 3:44:07 PM
+1class Solution {
+2    public int maxProfit(int[] prices) {
+3        int profit = 0;
+4
+5        for (int i = 1; i < prices.length; i++) {
+6            if (prices[i] > prices[i - 1]) {
+7                profit += prices[i] - prices[i - 1]; // take profit
+8            }
+9        }
+10
+11        return profit;
+12    }
+13}
+14
