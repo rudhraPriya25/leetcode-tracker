743294eb9cfeca2eda5b64a334626f62e61228e9
rudhrapriya_s <rudhrasirini2006@gmail.com>
2026-07-27 15:08:54 +0530

    Create 89-GrayCode

---
+++ 89-GrayCode/version/java/89-GrayCode_20260727_150850.java
@@ -0,0 +1,30 @@
+// Last updated: 7/27/2026, 3:08:50 PM
+1import java.util.*;
+2
+3class Solution {
+4    public List<List<Integer>> levelOrder(TreeNode root) {
+5        List<List<Integer>> result = new ArrayList<>();
+6        if (root == null) return result;
+7
+8        Queue<TreeNode> queue = new LinkedList<>();
+9        queue.add(root);
+10
+11        while (!queue.isEmpty()) {
+12            int size = queue.size();
+13            List<Integer> level = new ArrayList<>();
+14
+15            for (int i = 0; i < size; i++) {
+16                TreeNode node = queue.poll();
+17                level.add(node.val);
+18
+19                if (node.left != null) queue.add(node.left);
+20                if (node.right != null) queue.add(node.right);
+21            }
+22
+23            result.add(level);
+24        }
+25
+26        return result;
+27    }
+28}
+29
