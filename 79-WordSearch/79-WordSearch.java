41a4a181e0533c1d97d1bda18a0c3a92aad779ce
rudhrapriya_s <rudhrasirini2006@gmail.com>
2026-07-28 16:05:32 +0530

    Create 79-WordSearch

---
+++ 79-WordSearch/79-WordSearch.java
@@ -0,0 +1,41 @@
+// Last updated: 7/28/2026, 4:05:31 PM
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int i, int j, int index) {
+        // Base case: word completely matched
+        if (index == word.length()) return true;
+
+        // Boundary + mismatch check
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length 
+            || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        // Mark visited
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        // Explore neighbors
+        boolean found = dfs(board, word, i+1, j, index+1) ||
+                        dfs(board, word, i-1, j, index+1) ||
+                        dfs(board, word, i, j+1, index+1) ||
+                        dfs(board, word, i, j-1, index+1);
+
+        // Backtrack (restore cell)
+        board[i][j] = temp;
+
+        return found;
+    }
+}
