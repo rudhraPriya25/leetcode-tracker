4cbbd0b12809aad8790d7579a6c1133ff5b005ba
rudhrapriya_s <rudhrasirini2006@gmail.com>
2026-07-28 16:02:39 +0530

    Create 146-LruCache

---
+++ 146-LruCache/146-LruCache.java
@@ -0,0 +1,54 @@
+// Last updated: 7/28/2026, 4:02:38 PM
+class LRUCache {
+    private class Node {
+        int key, value;
+        Node prev, next;
+        Node(int k, int v) { key = k; value = v; }
+    }
+
+    private Map<Integer, Node> map;
+    private int capacity;
+    private Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+        Node node = map.get(key);
+        remove(node);
+        insert(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            remove(map.get(key));
+        }
+        if (map.size() == capacity) {
+            remove(tail.prev); // evict LRU
+        }
+        Node node = new Node(key, value);
+        insert(node);
+    }
+
+    private void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insert(Node node) {
+        map.put(node.key, node);
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+}
